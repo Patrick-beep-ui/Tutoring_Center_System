893ddc69a2a7a310697af0af764b50c24d78582e
Patrick Solis <patrickresol@gmail.com>
2024-06-13 17:54:17 -0600

    Routes Protected

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -4,6 +4,7 @@ import { createBrowserRouter, RouterProvider, Outlet, useOutletContext } from "r
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+//Views
 import Home from "./views/Home";
 import Tutors from "./views/Tutors";
 import AddTutor from "./views/AddTutor";
@@ -17,63 +18,72 @@ import Session from "./views/Sessions";
 import AddSession from "./views/AddSession";
 import Login from "./views/Login";
 
+//Components
+import Auth from "./components/Auth";
+
 //import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <Auth />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/tutors",
+        element: <Tutors />
+      },
+      {
+        path: "/tutors/add",
+        element: <AddTutor />
+      },
+      {
+        path: "/classes",
+        element: <ClassName />
+      },
+      {
+        path: "/classes/add",
+        element: <AddClass />
+      },
+      {
+        path: "/profile/:tutor_id",
+        element: <TutorProfile />
+      },
+      {
+        path: "/majors",
+        element: <Major />
+      },
+      {
+        path: "/majors/add",
+        element: <AddMajor />
+      },
+      {
+        path: "/terms/add",
+        element: <AddSemester />
+      },
+      {
+        path: "/sessions/:tutor_id/:course_id",
+        element: <Session />
+      },
+      {
+        path: "/sessions/add/:tutor_id/:course_id",
+        element: <AddSession />
+      }
+    ]
   },
   {
-    path: "/tutors",
-    element: <Tutors />
-
-  },
-  {
-    path: "/tutors/add",
-    element: <AddTutor />
-  }, 
-  {
-    path: "/classes",
-    element: <ClassName/>
-  }, 
-  {
-    path: "/classes/add",
-    element: <AddClass/>
-  }, 
-  {
-    path: "/profile/:tutor_id",
-    element: <TutorProfile/>
-  }, 
-  {
-    path: "/majors",
-    element: <Major/>
-  },
-  {
-    path: "/majors/add",
-    element: <AddMajor/>
-  }, {
-    path: "/terms/add",
-    element: <AddSemester/>
-  },
-  {
-    path: "/sessions/:tutor_id/:course_id",
-    element: <Session/>
-  },
-  {
-    path: "/sessions/add/:tutor_id/:course_id",
-    element: <AddSession/>
-  }, {
     path: "/login",
-    element: <Login/>
+    element: <Login />
   }
-
-], {basename: "/"});
+], { basename: "/" });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router}>
   <React.StrictMode>
-    <Home />
+    <Login />
   </React.StrictMode>
 </RouterProvider>
 );
