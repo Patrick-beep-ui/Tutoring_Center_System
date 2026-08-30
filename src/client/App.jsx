4a70efe30b22c8041c0c34f13347695607484838
icywest <icynwest@gmail.com>
2026-08-29 18:56:29 -0600

    refactor(styles): remove Bootstrap and consolidate Tailwind styling

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import { LayoutProvider } from './context/Layout';
-import './App.css';
 
 const App = () => {
   return (
     <div className="layout">
       <Header />
-      <div className="content">
+      <div>
         <Outlet /> 
       </div>
     </div>
--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
 import { Toaster } from 'sonner';
 
 // wrappers
