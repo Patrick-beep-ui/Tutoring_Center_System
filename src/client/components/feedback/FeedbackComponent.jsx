ddd747ba618543425111493ddc9f040a522a3b09
Patrick Solis <patrickresol@gmail.com>
2026-09-05 17:29:07 -0600

    refactor(feedback): completed feedback components domain

--- src/client/components/feedback/FeedbackComponent.jsx
+++ src/client/components/feedback/FeedbackComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import auth from '../authService';
+import auth from '../../authService';
 import { useNavigate } from 'react-router-dom';
 
 const FeedbackForm = ({sessionId, userId}) => {
